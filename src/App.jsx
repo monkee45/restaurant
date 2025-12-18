54d14b607c15dd776a1e1780d0bbe77534b4e505
Monkee45 <michael.tom.walsh@gmail.com>
2025-12-18 14:56:49 +1100

    added comment to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Menu from "./Menu"
-import Categories from './Categories'
-import items from "./data"
-import styles from './App.module.css'
-
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+import styles from "./App.module.css";
+// This is a simple app
 function App() {
   return (
     <main>
-      <section className={"${styles.menu} ${styles.section}"} >
+      <section className={"${styles.menu} ${styles.section}"}>
         <div className={styles.title}>
           <h2>our menu</h2>
           <div className={styles.underline}></div>
         </div>
         <Categories />
-        <Menu items={items}/>
+        <Menu items={items} />
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
